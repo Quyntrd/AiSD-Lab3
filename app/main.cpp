d49b1eec78a985cf086ff26550d9bb69f2023c02
Quyntrd <daniil.terebilkib@gmail.com@git config --global user.name Quyntrdgit config --global user.email daniil.terebilkib@gmail.com@>
2023-12-17 13:04:29 +0400

    added test for sorts

--- app/main.cpp
+++ app/main.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
 #include <random>
 #include <vector>
-#include <bubble_sort.cpp>
-#include <comb_sort.cpp>
-#include <shake_sort.cpp>
-#include <generate.cpp>
-#include <stats.cpp>
+#include <sorts.cpp>
 
+
+#define SIZE 1000;
 
 using namespace std;
 
 
 int main() {
-	return 0;
+	stats stat1;
+	vector<int> arr = { 1, 3, 5, 2, 20, 9, 10, 43, 4, 7 , 11};
+	vector<int> arr1 = { 1, 3, 5, 2, 20, 9, 10, 43, 4, 7 , 11};
+	vector<int> arr2 = { 1, 3, 5, 2, 20, 9, 10, 43, 4, 7 , 11};
+	//ѕроверка работоспособности сортировок
+	stat1 = bubble_sort(arr);
+	cout << "comp count " << stat1.comp_count << " copy count " << stat1.copy_count << endl;
+	stat1 = shake_sort(arr1);
+	cout << "comp count " << stat1.comp_count << " copy count " << stat1.copy_count << endl;
+	stat1 = comb_sort(arr2);
+	cout << "comp count " << stat1.comp_count << " copy count " << stat1.copy_count << endl;
+	for (int i = 0; i < arr.size(); ++i) cout << arr[i] << " ";
+	cout << endl;
+	for (int i = 0; i < arr1.size(); ++i) cout << arr1[i] << " ";
+	cout << endl;
+	for (int i = 0; i < arr2.size(); ++i) cout << arr2[i] << " ";
+	cout << endl;
 }
---
+++ include/sorts.cpp
@@ -0,0 +1,172 @@
+#include <vector>
+
+
+using namespace std;
+
+struct stats {
+	size_t comp_count = 0;
+	size_t copy_count = 0;
+
+	friend stats operator+(const stats& left, const stats& right)
+	{
+		stats stats;
+		stats.comp_count = left.comp_count + right.comp_count;
+		stats.copy_count = left.copy_count + right.copy_count;
+		return stats;
+	}
+	stats& operator=(const stats& right) {
+		comp_count = right.comp_count;
+		copy_count = right.copy_count;
+		return *this;
+	}
+};
+
+
+stats bubble_sort(vector<int>& vec) {
+	stats stat;
+
+	size_t length = vec.size();
+	for (size_t i = 0; i < length - i - 1; ++i) {
+		for (size_t j = 0; j < length - i - 1; ++j) {
+			if (vec[j] > vec[j + 1]) {
+				swap(vec[j + 1], vec[j]);
+				stat.copy_count += 1;
+			}
+			stat.comp_count += 1;
+		}
+	}
+	return stat;
+}
+
+
+template<class Iterator>
+stats bubble_sort_it(Iterator begin, Iterator end) {
+	stats stat;
+	for (Iterator i = begin; i != end; ++i) {
+		for (Iterator j = begin; j < i; ++j) {
+			if (*i < *j) {
+				iter_swap(i, j);
+				stat.copy_count += 1;
+			}
+			stat.copy_count += 1;
+		}
+	}
+	return stat;
+}
+
+stats shake_sort(vector<int>& vec) {
+	stats stat;
+	int left = 0, right = vec.size() - 1;
+	int flag = 1;
+	while (left < right && flag > 0) {
+		flag = 0;
+		for (int i = left; i < right; ++i) {
+			stat.comp_count += 1;
+			if (vec[i] > vec[i + 1]) {
+				swap(vec[i], vec[i + 1]);
+				flag = 1;
+				stat.copy_count += 1;
+			}
+		}
+		--right;
+		for (int i = right; i > left; --i) {
+			stat.comp_count += 1;
+			if (vec[i - 1] > vec[i]) {
+				swap(vec[i - 1], vec[i]);
+				flag = 1;
+				stat.copy_count += 1;
+			}
+		}
+		++left;
+	}
+	return stat;
+}
+
+template<typename Iterator>
+stats shake_sort_it(Iterator begin, Iterator end) {
+	stats stat;
+	auto left = begin, right = end - 1;
+	int flag = 1;
+	while (left < right && flag > 0) {
+		flag = 0;
+		for (auto i = left; i != right; ++i) {
+			stat.comp_count += 1;
+			if (*i > *(i + 1)) {
+				iter_swap(i, i + 1);
+				flag = 1;
+				stat.copy_count += 1;
+			}
+		}
+		--right;
+		for (auto i = right; i > left; --i) {
+			stat.comp_count += 1;
+			if (*(i - 1) > i) {
+				iter_swap(i - 1, i);
+				flag = 1;
+				stat.copy_count += 1;
+			}
+		}
+		++left;
+	}
+	return stat;
+}
+
+stats comb_sort(vector<int>& vec) {
+	stats stat;
+	auto n = vec.size();
+	auto gap = n;
+	bool flag = true;
+	while (gap != 1 || flag) {
+		gap = (gap * 10) / 13;
+		if (gap < 1) gap = 1;
+		flag = false;
+		for (int i = 0; i < n - gap; ++i) {
+			stat.comp_count += 1;
+			if (vec[i] > vec[i + gap]) {
+				swap(vec[i], vec[i + gap]);
+				stat.copy_count += 1;
+				flag = true;
+			}
+		}
+	}
+	return stat;
+}
+
+template <typename Iterator>
+stats comb_sort_it(Iterator begin, Iterator end) {
+	stats stat;
+	auto n = distance(begin, end);
+	auto gap = n;
+	bool flag = true;
+	while (gap > 1 || flag) {
+		gap = (gap * 10) / 13;
+		if (gap < 1)
+			gap = 1;
+		flag = false;
+		for (auto i = begin; i != end - gap; ++i) {
+			stat.comp_count += 1;
+			if (*i > *(i + gap)) {
+				iter_swap(i, i + gap);
+				stat.copy_count += 1;
+				flag = true;
+			}
+		}
+	}
+	return stat;
+}
+
+vector<int> generate_sorted(const int size) {
+	vector<int> result;
+	for (int i = 0; i < size; ++i) result.push_back(i);
+	return result;
+}
+vector<int> generate_inverted(const int size) {
+	vector<int> result;
+	for (int i = size - 1; i >= 0; --i) result.push_back(i);
+	return result;
+}
+vector<int> generate_random(const int size) {
+	vector<int> result;
+	for (int i = 0; i < size; ++i) result.push_back(rand() % size);
+	return result;
+}
